32e23ff84f6606a49b1d9b56be5642b5bdfb7fb7
pinhasyakubov <pinhas@bigid.co>
2020-03-29 21:37:24 +0300

    Updates

---
+++ precache-manifest.e36a3022452a9c779391d9457a5ca308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66e2547ddf814ca5c47a71c4a14aaf6",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "eec30637ad2c00270844",
+    "url": "/mosters-rolodex/static/css/main.fbeea473.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec30637ad2c00270844",
+    "url": "/mosters-rolodex/static/js/main.74bce447.chunk.js"
+  },
+  {
+    "revision": "3b1d11c5c06fc5abea79",
+    "url": "/mosters-rolodex/static/js/runtime-main.f637eaeb.js"
+  }
+]);
